6d1f981716f4cf97a5120fbba3ba42460f6f9512
k104h8KX <tom.avila@outlook.com>
2025-03-03 15:27:28 +0200

    Initialize emailer project with basic structure and configuration files

---
+++ emailer/bin/emailer.dart
@@ -0,0 +1,5 @@
+import 'package:emailer/emailer.dart' as emailer;
+
+void main(List<String> arguments) {
+  print('Hello world: ${emailer.calculate()}!');
+}
---
+++ emailer/test/emailer_test.dart
@@ -0,0 +1,8 @@
+import 'package:emailer/emailer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
